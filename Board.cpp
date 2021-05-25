3a3df106f049e88c4cb0b8afdb700c9831c4045f
Vinícius Reis <tiozinhodomiojo@hotmail.com>
2021-05-25 04:31:05 -0300

    board now rendering correct in a 8x8 pattern

--- Board.cpp
+++ Board.cpp
@@ -8,6 +8,53 @@ Board::Board()
 
 void Board::initBoard()
 {
+	int k = 0;
+
+	// INIT BLACK AREA
+	for (int j = 0; j < 8; j++)
+	{
+		k++;
+		int z = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			z++;
+			if (k % 2 == 0)
+			{
+				boardCubes[j][i * 2].setColor(0.35f, 0.24f, 0.13f);
+				boardCubes[j][i * 2].setPos(i * 3, 0.0f, j * 1.5);
+				boardCubes[j][i * 2].setSize(1.5f);
+			}
+			else
+			{
+				boardCubes[j][i + z].setColor(0.35f, 0.24f, 0.13f);
+				boardCubes[j][i + z].setPos((i * 3 + 1.5), 0.0, j * 1.5);
+				boardCubes[j][i + z].setSize(1.5f);
+			}
+		}
+	}
+
+	// ININT WHITE AREA
+	for (int j = 0; j < 8; j++)
+	{
+		k++;
+		int z = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			z++;
+			if (k % 2 == 0)
+			{
+				boardCubes[j][i + z].setColor(1.05f, 0.90f, 0.70f);
+				boardCubes[j][i + z].setPos((i * 3 + 1.5), 0.0, j * 1.5);
+				boardCubes[j][i + z].setSize(1.5f);
+			}
+			else
+			{
+				boardCubes[j][i * 2].setColor(1.05f, 0.90f, 0.70f);
+				boardCubes[j][i * 2].setPos(i * 3, 0.0f, j * 1.5);
+				boardCubes[j][i * 2].setSize(1.5f);
+			}
+		}
+	}
 }
 
 void Board::render()
